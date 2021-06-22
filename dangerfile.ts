f3c8ba0babd5da435dca8cbec4c8ebdf43a55b6e
Dragos Dumitrache <dragos@upsidetechnology.co>
2021-06-22 15:33:01 +0300

    Read large file

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,3 +3,12 @@ import { danger, markdown, message } from "danger";
 message("Changed Files in this PR: \n - " + "Hello World!";
 message("ENV Var check: \n - ";
 markdown("Hello There!")
+
+const fs = require('fs')
+const output = fs.readFile('/etc/hosts', 'utf8', function (err,data) {
+  return data.split('\n');
+};
+
+for (const line in output) {
+  markdown(line)
+}
